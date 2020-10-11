5a63c53da7b7849a7826cd1cc53cd5584dc8a000
Florian Soller <flsoller@gmail.com>
2020-10-11 19:57:41 +0200

    Add globalStyle and Theme with ThemeProvider

---
+++ gatsby-browser.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { ThemeProvider, createGlobalStyle } from "styled-components"
+import Theme from "./src/themes/theme"
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body, html {
+        font-family: ${props => props.theme.fonts.main};
+    }
+`
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+    {element}
+  </ThemeProvider>
+)
---
+++ src/themes/theme.js
@@ -0,0 +1,14 @@
+export default {
+  fonts: {
+    main: "Roboto, sans-serif",
+  },
+
+  colors: {
+    backgroundLight: "#F2F2F2",
+    backgroundBlue: "#3F3D56",
+  },
+
+  shadows: {
+    shadowButton: "0px 4px 4px 0px rgba(0,0,0,0.25)",
+  },
+}
