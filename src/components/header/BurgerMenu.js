0e2d064c3b881d9e0416e9eab31018c2d58cdc5d
Florian Soller <flsoller@gmail.com>
2020-10-17 16:29:17 +0200

    Implement open/close animation for icon.

--- src/components/header/BurgerMenu.js
+++ src/components/header/BurgerMenu.js
@@ -1,8 +1,37 @@
 import React from "react"
 import styled from "styled-components"
 
-const StyledBurgerMenu = styled.div`
+const StyledBurgerMenu = styled.button`
   cursor: pointer;
+  transition: all 0.3s linear;
+  background: transparent;
+  outline: none;
+  border: none;
+
+  div {
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+    :first-child {
+      transform: ${({ openMenu }) =>
+        openMenu ? "rotate(45deg)" : "rotate(0)"};
+    }
+
+    :nth-child(2) {
+      opacity: ${({ openMenu }) => (openMenu ? "0" : "1")};
+      transform: ${({ openMenu }) =>
+        openMenu ? "translateX(20px)" : "translateX(0)"};
+    }
+
+    :nth-child(3) {
+      transform: ${({ openMenu }) =>
+        openMenu ? "rotate(-45deg)" : "rotate(0)"};
+    }
+  }
+
+  @media (min-width: 768px) {
+    display: none;
+  }
 `
 
 const BurgerMenuLine = styled.div`
