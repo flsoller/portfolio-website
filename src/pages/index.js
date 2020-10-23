c33eb222112cd19f14c08c08b910d180ea7f145c
Florian Soller <flsoller@gmail.com>
2020-10-23 23:16:41 +0200

    Initial elements placement.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,68 @@
 import React from "react"
+import styled from "styled-components"
 
 // Component imports.
 import Navbar from "../components/header/Navbar"
+import Landing from "../layouts/LandingContainer"
+import {
+  UpToDateSVG,
+  MobileWebSVG,
+  CodeReviewSVG,
+  WebSearchSVG,
+} from "../assets/drawings"
+
+const StyledTagline = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px auto;
+  width: 85%;
+`
+
+const StyledStatement = styled.section`
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  width: 85%;
+`
+
+const StyledTextGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  width: 85%;
+`
+
+const StyledButton = styled.button`
+  width: 15rem;
+  height: 2rem;
+  outline: none;
+  border: none;
+  background-color: #3f3d56;
+  color: #f2f2f2;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <h1>Hello world!</h1>
+      <StyledTagline>
+        <p>Wir erstellen Ihren schnellen und modernen Internetauftritt.</p>
+      </StyledTagline>
+      <StyledStatement>
+        <StyledTextGroup>
+          <p>100% Mobile First.</p>
+          <p>Innovative Technologien.</p>
+        </StyledTextGroup>
+        <StyledButton>Kontaktieren Sie uns</StyledButton>
+      </StyledStatement>
+      <Landing>
+        <UpToDateSVG />
+        <CodeReviewSVG />
+      </Landing>
+      <MobileWebSVG />
+      <WebSearchSVG />
     </>
   )
 }
