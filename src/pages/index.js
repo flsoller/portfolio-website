44f9acc99e554e81b9a4d823b3f97f9d1568104e
Florian Soller <flsoller@gmail.com>
2020-10-24 20:57:07 +0200

    Implement mobile landing section.

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,6 +28,7 @@ const StyledStatement = styled.section`
 
 const StyledTextGroup = styled.div`
   display: flex;
+  font-size: 0.9rem;
   flex-direction: column;
   margin: 0 auto;
   width: 85%;
@@ -38,31 +39,68 @@ const StyledButton = styled.button`
   height: 2rem;
   outline: none;
   border: none;
-  background-color: #3f3d56;
-  color: #f2f2f2;
+  background-color: ${props => props.theme.colors.mainBlue};
+  color: ${props => props.theme.colors.mainLight};
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`
+
+const BottomSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  text-align: center;
+  margin: 0 auto;
+  height: 45%;
+  width: 100%;
+  background-color: ${props => props.theme.colors.mainBlue};
+  color: ${props => props.theme.colors.mainLight};
+
+  h3 {
+    margin-top: 0.5rem;
+    text-transform: uppercase;
+  }
+  h5 {
+    margin-top: 1rem;
+  }
+
+  button {
+    outline: none;
+    background: transparent;
+    border: none;
+    color: ${props => props.theme.colors.mainLightAlt};
+    margin-top: 0.5rem;
+  }
 `
 
 export default function Home() {
   return (
-    <>
+    <main>
       <Navbar />
-      <StyledTagline>
-        <p>Wir erstellen Ihren schnellen und modernen Internetauftritt.</p>
-      </StyledTagline>
-      <StyledStatement>
-        <StyledTextGroup>
-          <p>100% Mobile First.</p>
-          <p>Innovative Technologien.</p>
-        </StyledTextGroup>
-        <StyledButton>Kontaktieren Sie uns</StyledButton>
-      </StyledStatement>
       <Landing>
+        <StyledTagline>
+          <h1>Wir erstellen Ihren schnellen und modernen Internetauftritt.</h1>
+        </StyledTagline>
+        <StyledStatement>
+          <StyledTextGroup>
+            <p>100% Mobile First.</p>
+            <p>Innovative Technologien.</p>
+          </StyledTextGroup>
+          <StyledButton>Kontaktieren Sie uns</StyledButton>
+        </StyledStatement>
         <UpToDateSVG />
-        <CodeReviewSVG />
+        <BottomSection>
+          <h3>Was wir bieten</h3>
+          <h5>
+            Handgeschriebene Webseiten ab 150€ / Monat. <br /> Kein Wordpress.
+            Keine Pagebuilder.
+          </h5>
+          <button>mehr erfahren...</button>
+          <CodeReviewSVG />
+        </BottomSection>
       </Landing>
       <MobileWebSVG />
       <WebSearchSVG />
-    </>
+    </main>
   )
 }
