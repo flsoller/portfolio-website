1557a0144ee83204e46f0f165b4adfdc0b8a0f16
Florian Soller <flsoller@gmail.com>
2021-03-27 15:27:16 +0100

    Reset repo. Initialize with NextJS, TS, Tailwind.

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <button className="py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-400">
+      Hello world
+    </button>
+  );
+}
