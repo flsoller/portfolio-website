1ec3e64fe47ebcc66c85308fa1535fb963c56b6a
Florian Soller <flsoller@gmail.com>
2021-04-10 09:51:12 +0200

    Extend default colors with bg-color.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgBlue: '#E4ECF4',
+      },
+    },
   },
   variants: {
     extend: {},
