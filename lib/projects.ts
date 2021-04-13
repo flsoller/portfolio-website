ede9353849c71f57f444f19f2b53c3150e61f4f9
Florian Soller <flsoller@gmail.com>
2021-04-13 18:27:11 +0200

    Add project.

--- lib/projects.ts
+++ lib/projects.ts
@@ -19,6 +19,14 @@ const projectCollection: Project[] = [
     hostedLink: 'https://flsoller.github.io/library-react-app/',
     sourceLink: 'https://github.com/flsoller/library-react-app/',
   },
+  {
+    imgSource: `${imgSource}/weather-app-project.png`,
+    name: 'Weather App',
+    description:
+      'Uses ReactJS and Styled-Components. Implements OpenWeatherMap API calls with Async/Await syntax.',
+    hostedLink: 'https://flsoller.github.io/weather-app/',
+    sourceLink: 'https://github.com/flsoller/weather-app',
+  },
 ];
 
 export default projectCollection;
