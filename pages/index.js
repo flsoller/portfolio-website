5e4f57e8e87a79f36cd3c24f5f02e9c11d95b52e
Florian Soller <flsoller@gmail.com>
2021-04-20 19:44:12 +0200

    Create initial mobile index page.

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <button className="py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-400">
-      Hello world
-    </button>
-  );
-}
