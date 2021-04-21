63121ccd863dbe617e9c85b9bfe169374bf34b9a
Florian Soller <flsoller@gmail.com>
2021-04-21 18:13:28 +0200

    Create footer component.

---
+++ components/Footer.tsx
@@ -0,0 +1,33 @@
+import { FaTwitter, FaLinkedinIn, FaHeart } from 'react-icons/fa';
+
+export default function Footer() {
+  return (
+    <div className="flex flex-col p-6 items-center text-center">
+      <div className="flex flex-row justify-center content-center">
+        <p>Made with</p>
+        <FaHeart className="text-red-500 m-1 mx-2" />
+        <p>in Berlin.</p>
+      </div>
+
+      <p>You can connect with me on Twitter and LinkedIn</p>
+      <div className="flex flex-row space-x-2 mt-2">
+        <a
+          className="bg-blue-400 text-white px-3 py-2 text-center rounded-md text-base font-medium"
+          target="_blank"
+          rel="noopener"
+          href="https://twitter.com/Florian_FSO"
+        >
+          <FaTwitter />
+        </a>
+        <a
+          className="bg-blue-400 text-white px-3 py-2 text-center rounded-md text-base font-medium"
+          target="_blank"
+          rel="noopener"
+          href="https://www.linkedin.com/in/floriansoller/"
+        >
+          <FaLinkedinIn />
+        </a>
+      </div>
+    </div>
+  );
+}
