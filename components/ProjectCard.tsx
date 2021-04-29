69b2600774a2d48e3e44b63d9421e72713277b47
Florian Soller <flsoller@gmail.com>
2021-04-29 18:48:24 +0200

    Wrap demo link button in conditional statement.

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,7 +4,14 @@ import ProjectFrame from '../components/ProjectFrame';
 import { Project } from '../lib/types';
 
 export default function ProjectCard(props: Project) {
-  const { imgSource, name, description, hostedLink, sourceLink } = props;
+  const {
+    imgSource,
+    name,
+    description,
+    hostedLink,
+    sourceLink,
+    noDemo,
+  } = props;
 
   return (
     <article className="flex flex-col lg:flex-row lg:even:flex-row-reverse p-4 lg:p-8 items-center max-w-2xl lg:max-w-6xl lg:space-x-4 align-center">
@@ -22,14 +29,16 @@ export default function ProjectCard(props: Project) {
         <h1 className="font-bold">{name}</h1>
         <p className="text-base">{description}</p>
         <section className="font-medium flex space-x-6 justify-center lg:justify-start">
-          <a
-            className="bg-gray-700 text-white px-3 py-2 text-center rounded-md text-base font-medium"
-            target="_blank"
-            rel="noopener"
-            href={hostedLink}
-          >
-            Live Demo
-          </a>
+          {noDemo ? null : (
+            <a
+              className="bg-gray-700 text-white px-3 py-2 text-center rounded-md text-base font-medium"
+              target="_blank"
+              rel="noopener"
+              href={hostedLink}
+            >
+              Live Demo
+            </a>
+          )}
           <a
             className="bg-gray-700 text-white px-3 py-2 text-center rounded-md text-base font-medium"
             target="_blank"
