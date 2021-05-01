509ce618a2d405803a3c06a124f62c2c9e3ec60d
Florian Soller <flsoller@gmail.com>
2021-05-01 21:23:39 +0200

    Implement preview component and static source.

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,12 +1,42 @@
+import { GetStaticProps } from 'next';
+import { ContentfulClientApi, createClient } from 'contentful';
 import Layout from '../components/Layout';
+import PostPreview from '../components/PostPreview';
 
-export default function Blog() {
+export const getStaticProps: GetStaticProps = async () => {
+  const client: ContentfulClientApi = createClient({
+    space: String(process.env.CONTENTFUL_SPACE_ID),
+    accessToken: String(process.env.CONTENTFUL_ACCESS_KEY),
+  });
+
+  const res = await client.getEntries({ content_type: 'blogPost' });
+
+  return {
+    props: {
+      blogPost: res.items,
+    },
+  };
+};
+
+export default function Blog({ blogPost }: any) {
   return (
     <Layout title="Florian Soller - Blog">
-      <div className="flex justify-center mt-20">
-        <p className="text-xl font-bold text-gray-800">
-          This page is currently under construction.
-        </p>
+      <div className="flex flex-col justify-center items-center mt-8 mx-auto space-y-6 md:max-w-5xl p-4 md:p-8">
+        <ul className="divide-y divide-gray-300">
+          {blogPost.map((post: any) => {
+            return (
+              <li className="py-12" key={post.sys.id}>
+                <PostPreview
+                  date={post.fields.createdDate}
+                  title={post.fields.title}
+                  tags={post.fields.tag}
+                  description={post.fields.description}
+                  slug={post.fields.slug}
+                />
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </Layout>
   );
