b4a3d6203fe0b0105b291defc1cae55fc6fe4932
Florian Soller <flsoller@gmail.com>
2023-05-28 20:35:55 +0200

    Remove date from blog post preview

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -23,9 +23,9 @@ export default function Blog({ blogPost }: any) {
   return (
     <Layout
       title="Florian Soller - Blog"
-      description="A blog to document my journey through web development as well as write about my projects and the technolgies I use"
+      description="A blog to document my journey through web development as well as write about my projects and the technologies I use"
     >
-      <div className="flex flex-col justify-center items-center mt-8 mx-auto space-y-6 md:max-w-5xl p-4 md:p-8">
+      <div className="flex flex-col justify-center items-center mt-8 mx-auto space-y-6 md:max-w-5xl p-8">
         <ul className="divide-y divide-gray-300">
           {blogPost.map((post: any) => {
             return (
